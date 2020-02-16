8f479f0ce41402a9d3418b1111a34cfe0bc56c6e
DeLaut <54510364+DeLaut@users.noreply.github.com>
2020-02-16 15:01:57 +0300

    added: second slider Test2

--- Test2/slider.js
+++ Test2/slider.js
@@ -2,49 +2,49 @@ const slides = [
     {
         data_id: "0",
         title: "Sees!",
-        picture: "./images/mag01.jpg",
+        picture: "../images/mag01.jpg",
         content: "Voluptate elit elit ipsum velit ea ipsum consequat in anim eiusmod. Veniam cupidatat consequat laboris commodo qui amet ea aliquip laborum. Anim Lorem eu voluptate eiusmod cillum reprehenderit reprehenderit sint aliqua id. Mollit culpa occaecat ea labore pariatur laboris tempor aute est excepteur sunt. Magna in eiusmod ex eiusmod reprehenderit et do quis pariatur nisi. Laboris laboris id eu amet elit elit excepteur commodo commodo eiusmod ipsum ullamco.",
     },
     {
         data_id: "1",
         title: "Forests!",
-        picture: "./images/mag02.jpg",
+        picture: "../images/mag02.jpg",
         content: "Aute proident adipisicing Lorem id duis fugiat. Reprehenderit eiusmod ullamco elit sit fugiat ex non deserunt. Proident tempor irure qui dolore Lorem minim nulla aute Lorem tempor non anim.",
     },
     {
         data_id: "2",
         title: "Sunsets!",
-        picture: "./images/mag03.jpg",
+        picture: "../images/mag03.jpg",
         content: "Ullamco occaecat reprehenderit dolore. Excepteur sint id sint dolore dolore nulla ex reprehenderit nostrud. Officia ea labore cupidatat officia id eu labore qui ullamco exercitation do ut consequat. Eu commodo exercitation in eu irure.",
     },
     {
         data_id: "3",
         title: "Castles!",
-        picture: "./images/mag04.jpg",
+        picture: "../images/mag04.jpg",
         content: "Ullamco ut voluptate minim Lorem. reprehenderit anim sunt pariatur. Lorem est elit pariatur sint duis voluptate elit eiusmod. Deserunt proident excepteur consequat non. Adipisicing dolore nulla mollit esse Lorem consectetur cupidatat ea aliqua nisi amet incididunt.",
     },
     {
         data_id: "4",
         title: "Roads!",
-        picture: "./images/mag05.jpg",
+        picture: "../images/mag05.jpg",
         content: "Nostrud dolor ut id ex nisi officia aliquip cillum aliqua reprehenderit anim sunt pariatur. Lorem est elit pariatur sint duis voluptate elit eiusmod. Deserunt proident excepteur consequat non. Adipisicing dolore nulla mollit esse Lorem consectetur cupidatat ea aliqua nisi amet incididunt.",
     },
     {
         data_id: "5",
         title: "Mountains!",
-        picture: "./images/mag06.jpg",
+        picture: "../images/mag06.jpg",
         content: "Nostrud dolor ut id ex nisi officia aliquip cillum aliqua reprehenderit anim sunt pariatur. Lorem est elit pariatur sint duis voluptate elit eiusmod. Deserunt proident excepteur consequat non. Adipisicing dolore nulla mollit esse Lorem consectetur cupidatat ea aliqua nisi amet incididunt.",
     },
     {
         data_id: "6",
         title: "Lakes!",
-        picture: "./images/mag07.jpg",
+        picture: "../images/mag07.jpg",
         content: "Nostrud dolor ut id ex nisi officia aliquip cillum aliqua reprehenderit anim sunt pariatur. Lorem est elit pariatur sint duis voluptate elit eiusmod. Deserunt proident excepteur consequat non. Adipisicing dolore nulla mollit esse Lorem consectetur cupidatat ea aliqua nisi amet incididunt.",
     },    
     {
         data_id: "7",
         title: "Rocks!",
-        picture: "./images/mag08.jpg",
+        picture: "../images/mag08.jpg",
         content: "Nostrud dolor ut id ex nisi officia aliquip cillum aliqua reprehenderit anim sunt pariatur. Lorem est elit pariatur sint duis voluptate elit eiusmod. Deserunt proident excepteur consequat non. Adipisicing dolore nulla mollit esse Lorem consectetur cupidatat ea aliqua nisi amet incididunt.",
     }
 ]
@@ -52,11 +52,8 @@ const slides = [
 const slider = document.querySelector('.slider');
 let sliderHtml = "";
 let swipeLength = 0;
-let elementWidth = 0;
-
-let currentElementIndex = 1;
-let leftElementIndex = 0;
-let rightElementIndex = 2;
+let sliderHalfWidth = slider.offsetWidth/2;
+let visibleItems = 3;
 
 slides.forEach( (item) => {
     sliderHtml += `
@@ -70,68 +67,34 @@ slides.forEach( (item) => {
 slider.innerHTML += sliderHtml;
 
 const sliderItems = document.querySelectorAll('.slider__item');
+let elementWidth = sliderItems[0].offsetWidth;
 
-// let elementWidth = sliderItems[0].offsetWidth;
-// let elementWidth = sliderItems[0].getBoundingClientRect().width;
+document.addEventListener('keydown', (event) => {
+    if (event.keyCode == 37 && swipeLength != 0) {
+        swipeLeft();
+    }
+
+    if (event.keyCode == 39 && swipeLength != -elementWidth * (sliderItems.length - visibleItems)) {
+        swipeRight();
+    }
+})
 
 slider.addEventListener('click', (event) => {
-        elementWidth = sliderItems[0].offsetWidth;
-        
-        let selectedItemIndex = getSelectedItemIndex(event);
+        let eventX = event.clientX;
+        // console.log(sliderHalfWidth);
+        // console.log(eventX);
+        // console.log(slider.offsetWidth);
+        // console.log(event);
+        // console.log(event.target);
 
-        if (selectedItemIndex == undefined) {
-            return false;
-        };
-    
-        if (selectedItemIndex == currentElementIndex) {
-            return false;
-        };
-
-        if (selectedItemIndex == 0) {
-            return false;
-        };
-
-        if (selectedItemIndex == sliderItems.length - 1) {
-            return false;
-        };
-
-        if (selectedItemIndex == leftElementIndex) {
+        if (eventX <= sliderHalfWidth && swipeLength != 0) {
             swipeLeft();
         }
 
-        if (selectedItemIndex == rightElementIndex) {
+        if (eventX > sliderHalfWidth && swipeLength != -elementWidth * (sliderItems.length - visibleItems)) {
             swipeRight();
         }
-
-        leftElementIndex = selectedItemIndex - 1;
-        rightElementIndex = selectedItemIndex + 1;
-        currentElementIndex = selectedItemIndex;
-        console.log(currentElementIndex);
 })
-
-function getSelectedItemIndex(event) {
-    let e = event.target;
-    let index;
-
-    if (e == slider || (e.parentElement == slider && e.getAttribute('id') == null)) {
-        return index;
-    }
-
-    if (e.parentElement === slider) {
-        index = Number(e.getAttribute('id'));
-    }
-
-    while (e.parentElement != slider) {
-
-        e = e.parentElement;
-
-        if (e.parentElement == slider) {
-            index = Number(e.getAttribute('id'));
-        }
-    }
-
-    return index;
-}
 
 function swipeLeft() {
     swipeLength += elementWidth;
